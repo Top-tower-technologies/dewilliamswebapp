870ac39defb2cbcae28b20c202e68753a863c311
stepheeeen <stephenon56@gmail.com>
2025-03-10 07:46:20 +0100

    add apartments header component and update layout with new SuperiorRoomCard configurations

--- src/app/apartments/page.tsx
+++ src/app/apartments/page.tsx
@@ -1,10 +1,10 @@
-import Header from "@/component/pages/home/Header";
 import { FooterLayout } from "@/component/reusable/footer";
 import Navbar from "@/component/reusable/navbar";
 import SuperiorRoomCard from "@/component/reusable/SuperiorRoomCard";
 import room from "../../../public/bg/exclusivebg.png";
 import room1 from "../../../public/bg/roomsbg.png";
 import React from "react";
+import Header from "@/component/pages/apartments/Header";
 
 const page = () => {
   return (
@@ -12,11 +12,11 @@ const page = () => {
       <Navbar />
       <Header />
       <div className="py-10 px-30 pt-20 bg-gray-100">
-        <SuperiorRoomCard image={room1} reverse={false} />
+        <SuperiorRoomCard image={room1} reverse={true} />
       </div>
       <div className="space-y-20 py-20 bg-gray-100">
-        <SuperiorRoomCard image={room} reverse={true} />
         <SuperiorRoomCard image={room} reverse={false} />
+        <SuperiorRoomCard image={room} reverse={true} />
       </div>
       <FooterLayout />
     </div>
